f7331e56acec80d20a3009ce458e672870236584
acareaga <aaroncareaga@gmail.com>
2015-09-28 11:42:20 -0600

    Built out word finder for the module one assessment. Scored 3 on all categories.

--- lib/word_finder.rb
+++ lib/word_finder.rb
@@ -2,5 +2,18 @@ class WordFinder
   def words
     File.read('/usr/share/dict/words').split("\n")
   end
-  
+
+  def for_letters(letters)
+    words.select do |word|
+      can_build_word_from_letters?(word, letters)
+    end
+  end
+
+  def can_build_word_from_letters?(word, letters)
+    # [:all?, :any?, :one?, :none?, :member?, :include?]
+    word.chars.all? do |char|
+      letters.include?(char)
+    end
+    # word can be built from letters
+  end
 end
--- test/word_finder_test.rb
+++ test/word_finder_test.rb
@@ -6,4 +6,10 @@ require 'pry'
 
 class WordFinderTest < Minitest::Test
 
+  def test_for_letters_returns_all_words_in_the_dictionary_for_words_where_all_its_letters_are_in_the_given_array
+    finder = WordFinder.new
+    assert_equal ["a", "aa"], finder.for_letters(["a"])
+    assert_equal ["a", "aa", "at", "atta", "t", "ta", "taa", "tat", "tatta"], finder.for_letters(["a", "t"])
+    assert_equal 461, finder.for_letters(["r", "e", "x", "a", "p", "t", "o"]).count
+  end
 end
